8ae7f7ba5f35d0b64c9405c147b56b28bda438e4
Damola Mabogunje <damola@mabogunje.net>
2022-07-01 21:59:42 +0100

    [Update]: Updated Grav to v1.7.34

---
+++ cache/compiled/files/9c6951e9b4aa703a60c3945ea74dd91a.yaml.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/damola/Sites/www/user/plugins/markdown-notices/markdown-notices.yaml',
+    'modified' => 1606913248,
+    'size' => 99,
+    'data' => [
+        'enabled' => true,
+        'built_in_css' => true,
+        'base_classes' => 'notices',
+        'level_classes' => [
+            0 => 'yellow',
+            1 => 'red',
+            2 => 'blue',
+            3 => 'green'
+        ]
+    ]
+];
---
+++ cache/compiled/files/b146a6f9ea45e4f4e673268aec195239.yaml.php
@@ -0,0 +1,22 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/damola/Sites/www/user/themes/quark/quark.yaml',
+    'modified' => 1602073826,
+    'size' => 217,
+    'data' => [
+        'enabled' => true,
+        'production-mode' => true,
+        'grid-size' => 'grid-lg',
+        'header-fixed' => true,
+        'header-animated' => true,
+        'header-dark' => false,
+        'header-transparent' => false,
+        'sticky-footer' => true,
+        'blog-page' => '/blog',
+        'spectre' => [
+            'exp' => false,
+            'icons' => false
+        ]
+    ]
+];
